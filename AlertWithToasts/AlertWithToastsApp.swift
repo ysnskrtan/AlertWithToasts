0cdb14c7e772e531b485d1dcc449b1cde13050a9
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-12-13 12:41:56 +0300

    Initial Commit

---
+++ AlertWithToasts/AlertWithToastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertWithToastsApp.swift
+//  AlertWithToasts
+//
+//  Created by Yasin Şükrü Tan on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AlertWithToastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
